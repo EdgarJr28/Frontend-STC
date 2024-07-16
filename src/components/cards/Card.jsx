617dbb3f0b4e5105aed922b0c77dc3cb5613122f
Edgar Maldonado <ed.dev28@gmail.com>
2024-07-16 00:13:03 -0500

    showArticle responsive fixed

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 const Card = ({ title, img, author, description, onClick }) => {
     return (
         <a onClick={onClick}>
-            <div className="max-w-96 sm:max-w-[613px] mx-auto bg-white shadow-md rounded-lg overflow-hidden my-4 border-2 border-gray-200">
-                <div className="flex items-center px-8 p-4">
-                    <div className="w-24 h-24 bg-gray-200 rounded-xl flex-shrink-0">
+            <div className="max-w-96 sm:max-w-[613px] mx-auto bg-white shadow-md rounded-lg overflow-hidden my-4 border-2 border-gray-200 p-4">
+                <div className="flex flex-col sm:flex-row items-center px-4 sm:p-4 cursor-pointer">
+                    <div className="w-24 h-24 bg-gray-200 rounded-xl flex-shrink-0 mb-4 sm:mb-0">
                         <img src={img} alt="Article" className="object-cover w-full h-full rounded-xl" />
                     </div>
-                    <div className="ml-4">
+                    <div className="sm:ml-4 text-center sm:text-left">
                         <h2 className="text-xl font-bold">{title}</h2>
                         <p className="text-gray-600">{author}</p>
                         <p className="text-gray-700 mt-2">{description}</p>
                     </div>
                 </div>
             </div>
+
         </a>
     )
 }
--- src/components/cards/ShowArticle.jsx
+++ src/components/cards/ShowArticle.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ModalContainer from '../Modals/ModalContainer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 const ShowArticle = ({ data, onClose }) => {
     return (
@@ -7,16 +9,23 @@ const ShowArticle = ({ data, onClose }) => {
             <div className="px-4">
                 <h2 className="text-xl font-bold mb-2">{data.title}</h2>
                 <div className="flex justify-center">
-                    <img src={data.urlToImage} alt={data.title} className="max-w-full h-80 rounded-lg shadow-lg" />
+                    <img src={data.urlToImage} alt={data.title} className="max-w-full h-46 sm:h-80 rounded-lg shadow-lg" />
                 </div>
-                <p className="text-gray-700 w-[80%] mx-auto m-4 p-10">{data.description}</p>
-                <div className="absolute bottom-0 right-0 m-4">
-                    <div className="flex items-center mt-1">
-                        <p className="text-gray-600 text-sm font-semibold">Publicado por {data.author} &nbsp;</p>
-                        <p className="bg-gray-600 bg-opacity-50 text-gray-700 rounded-lg px-3 py-1 text-sm">{new Date(data.publishedAt).toLocaleDateString()}</p>
+
+                <p className="text-gray-700 text-sm mt-4 mb-6">{data.description}</p>
+
+                <div className="flex justify-between items-center">
+                    <a href={data.url} target="_blank" className="text-blue-600 text-sm font-semibold flex items-center">
+                        Go to site <FontAwesomeIcon icon={faArrowAltCircleRight} className="ml-1" />
+                    </a>
+
+                    <div className="text-gray-600 text-sm p-2">
+                        <p>Publicado por {data.author}</p>
+                        <p className="text-center bg-gray-600 bg-opacity-50 rounded-lg px-3 py-1 mt-1">{new Date(data.publishedAt).toLocaleDateString()}</p>
                     </div>
                 </div>
             </div>
+
         </ModalContainer>
     )
 }
