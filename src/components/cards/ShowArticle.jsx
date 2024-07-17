7807214ff546ddb500f9831feebac276dd087c5d
Edgar Maldonado <ed.dev28@gmail.com>
2024-07-16 20:01:06 -0500

    fix img size in modal showArticle

--- src/components/cards/ShowArticle.jsx
+++ src/components/cards/ShowArticle.jsx
@@ -5,11 +5,11 @@ import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 const ShowArticle = ({ data, onClose }) => {
     return (
-        <ModalContainer onClose={onClose}>
-            <div className="p-4 ">
+        <ModalContainer className={`sm:w-[60%]`} onClose={onClose}>
+            <div className="p-4">
                 <h2 className="text-xl text-center font-semibold mb-2 text-black p-2">{data.title}</h2>
                 <div className="flex justify-center">
-                    <img src={data.urlToImage} alt={data.title} className="max-w-full h-46 sm:h-80 rounded-lg shadow-lg" />
+                    <img src={data.urlToImage} alt={data.title} className="max-w-full w-46 h-46 sm:h-80 rounded-lg shadow-lg bg-cover " />
                 </div>
                 <p className="text-gray-700 text-sm mt-4 mb-6">{data.description}</p>
 
@@ -19,7 +19,7 @@ const ShowArticle = ({ data, onClose }) => {
                     </a>
 
                     <div className="text-gray-600 text-sm p-2">
-                        <p>Publicado por {data.author}</p>
+                        <p>Publicado por <span className="font-bold">{data.author}</span></p>
                         <p className="text-center bg-gray-600 bg-opacity-50 rounded-lg px-3 py-1 mt-1">{new Date(data.publishedAt).toLocaleDateString()}</p>
                     </div>
                 </div>
